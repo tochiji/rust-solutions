a6ed6ea2348b6c916f8c6cac86db0cefede80eec
tochiji <7910558+tochiji@users.noreply.github.com>
2024-04-23 03:35:17 +0900

    echor

--- echor/src/main.rs
+++ echor/src/main.rs
@@ -1,3 +1,21 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(version = "0.1.0", about = "Rust echo")]
+struct Cli {
+    /// Input text
+    #[arg(required = true)]
+    text: Vec<String>,
+
+    /// Do not print newline
+    #[arg(short = 'n')]
+    omit_newline: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    let text = cli.text.join(" ");
+    let omit_newline = if cli.omit_newline { "" } else { "\n" };
+    print!("{}{}", text, omit_newline);
 }
